c1f4adcf4ad203527f7d6c36bd551a8f7a7a6277
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-03-31 15:11:32 +0700

    memasukkan void input dari panjang element array dan maksimum array 20

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -7,5 +7,18 @@ int cap_count = 0; //number of comparasion
 int mov_count = 0; //number of data movement
 int n;
 
+void input() {
+	while (true)
+	{
+		cout << "Masukkan panajng element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+
+
 
 
